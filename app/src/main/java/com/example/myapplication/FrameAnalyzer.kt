48e9e4ded95ec36ee31da80a18304331930b701f
aszilard99 <aszilard991@gmail.com>
2020-12-08 19:45:09 +0200

    First Commit

---
+++ app/src/main/java/com/example/myapplication/FrameAnalyzer.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Context
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import kotlin.collections.ArrayList
+
+
+class FrameAnalyser(private var context: Context) : ImageAnalysis.Analyzer {
+
+    //itt lesznek eltárolva a nevek és hozzá tartozó arcok vektorleképezései
+    var faceList = ArrayList<Pair<String, FloatArray>>()
+
+    //ide kell berakni a modellt
+    private val model = null
+
+    //az analyzeban kell megadni hogy mit hajtson végre a FrameAnalyzer mindegyik kamera képre ,amit megkap
+    override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Size
+import android.view.TextureView
+import android.view.ViewGroup
+import androidx.camera.core.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var frameAnalyser  : FrameAnalyser
+    private lateinit var cameraTextureView : TextureView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //inicializálja a kamerát a camerax-es codelab szerint
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(640, 480))
+            setLensFacing(CameraX.LensFacing.FRONT)
+        }.build()
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = cameraTextureView.parent as ViewGroup
+            parent.removeView(cameraTextureView)
+            parent.addView(cameraTextureView, 0)
+
+            //kell egy függvény, ami akkor hívódik meg, ha elfordul a képernyő
+        }
+    }
+}
+
